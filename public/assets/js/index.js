f4b5aaf4226a27997e04fd149fbf1e0d5252062d
Vetrivel Ravi <vetrivel.galaxy@gmail.com>
2024-03-30 11:17:58 +0530

    Changes : Project Completed

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,5 +1,13 @@
 const mobileNav = document.getElementById("mobileNav");
 const toggleIcon = document.getElementById("toggleIcon");
+const listTags = mobileNav.querySelectorAll("ul li");
+
+listTags.forEach((tag, index) => {
+  tag.addEventListener("click", (index) => {
+    mobileNav.classList.toggle("right-0");
+    mobileNav.classList.toggle("-right-[100%]");
+  });
+});
 
 toggleIcon.addEventListener("click", () => {
   mobileNav.classList.toggle("right-0");
